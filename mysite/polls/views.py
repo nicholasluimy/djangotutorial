7cd06ec71c00da6dacf3596032352411f271015f
Nicholas Lui <nicholasluimy@gmail.com>
2015-09-09 16:01:01 +0800

    implemented testcases

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
